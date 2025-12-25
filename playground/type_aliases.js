61c329dc04daa7839fb9508c821507736088a473
Sabrine Drira <sabrined@kpit.com>
2025-12-25 12:19:05 +0100

    delete unnecessary file

--- playground/type_aliases.js
+++
@@ -1,5 +0,0 @@
-// Exercice lines.
-// let Coord = codecademyCoordinates = bermudaTCoordinates;
-// Now, replace the old, long type annotations for codecademyCoordinates and bermudaTCoordinates with annotations that use your new type alias.
-var codecademyCoordinates = [40, 43.2, 'N', 73, 59.8, 'W'];
-var bermudaTCoordinates = [25, 0, 'N', 71, 0, 'W'];
